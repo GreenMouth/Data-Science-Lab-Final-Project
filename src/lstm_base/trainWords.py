d5dc80776294541f7d8b3038672263c403bafa8a
Corey Hulse <cohulse@asu.edu>
2018-04-28 11:44:31 -0500

    edited length of generated sequence stuff

--- src/lstm_base/trainWords.py
+++ src/lstm_base/trainWords.py
@@ -4,13 +4,13 @@ from lstmTesting import *
 
 if __name__ == "__main__":
     allScripts = getData()
-    transcript = ' '.join(allScripts[:3])
+    transcript = ' '.join(allScripts[:5])
     #transcript = allScripts[0]
     uniqueWords = sorted(transcript.split(' '))
     numUniqueWords = len(uniqueWords)
     stringToInt = dict((string, i) for i, string in enumerate(uniqueWords))
 
-    lengthOfSequence = 25
+    lengthOfSequence = 4
     words = transcript.split(" ")
     data, targets = prepSequences(words, stringToInt, lengthOfSequence)
     preppedX = prepX(data, lengthOfSequence, numUniqueWords)
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     intToString = dict((i, word) for i, word in enumerate(uniqueWords))  #creating a demapping of our original encoding
     seed = generateSeedFromData(data) #get a random starting point from our paper and let the network continue the writing
-    numWords= 5   #length of each window the network will use to predict the output
+    numWords= 500   #length of each window the network will use to predict the output
     text = generateText(model, seed, intToString, length= numWords, vocabSize= numUniqueWords, delimeter= ' ')
     with open("generated_words.txt", 'w+') as file:
         file.write(text)
